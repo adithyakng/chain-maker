e44213997e93b5b6d96fc368ceadd49a986839f0
sid <siddhantkalgutkar@gmail.com>
2024-01-19 21:02:01 -0500

    Added documentation

--- makeChain.py
+++ makeChain.py
@@ -49,7 +49,15 @@ class AttackDB:
         return "AttackDB: " + ", ".join(attack_names)
 
 
-
+"""
+Algorithm Steps:
+1) The function starts by finding all attacks in the database that match the initial state.
+2) For each matching attack, the function checks if the current knowledge is a superset
+of the information required for the attack.
+3) If it is, the function logs that the attack is possible and adds the attack to a chain of attacks.
+4) The end state of the attack is set as the initial state for the next attack, before recursively 
+calling the function again.
+"""
 def makeChain(initState, attackDB, knowledge=set()):
     logging.info('Starting makeChain with initState: %s', initState.params)
     chains = []
@@ -58,7 +66,6 @@ def makeChain(initState, attackDB, knowledge=set()):
 
     # Find attacks with matching initState and knowledge
     matchingAttacks = [attack for attack in attackDB.attacks if attack.initState.equals(initState)]
-                    #    and knowledge.issuperset(attack.info_required)]
 
     logging.info('Found %d matching attacks', len(matchingAttacks))
     for attack in matchingAttacks:
@@ -79,11 +86,11 @@ def makeChain(initState, attackDB, knowledge=set()):
             # skip this attack and continue with the next one in the loop
             continue
 
-        # Set the initState for the next attack to be the endState of the current attack
-        initState = attack.endState
-
         # Update the knowledge with the information gained from the current attack
         knowledge.update(attack.info_gained)
+
+        # Set the initState for the next attack to be the endState of the current attack
+        initState = attack.endState
 
         # Recursively find chains starting from the endState
         subChains = makeChain(initState, attackDB, knowledge)
