8ce64ec7585d58794c93c83e748242331125a0c7
sid <siddhantkalgutkar@gmail.com>
2024-01-30 20:00:28 -0500

    API refactoring

--- NVD/api.py
+++ NVD/api.py
@@ -3,6 +3,8 @@ import json
 
 API_KEY = "99d5cd7a-70df-4fa5-bd38-d43b53da848c"
 RATE_LIMIT = 0.7
+INPUT_FILE = './attackDB.json'
+OUTPUT_FILE = './attackDB-final.json'
 
 
 def read_json(filename):
@@ -11,37 +13,32 @@ def read_json(filename):
     return data
 
 
-data = read_json('./attackDB.json')
-
-final_data = []
-
-
 def addToDatabase(CVE_ID):
-    r = nvdlib.searchCVE(cveId=CVE_ID, key=API_KEY, delay=RATE_LIMIT)[0]
-    return r
+    try:
+        r = nvdlib.searchCVE(cveId=CVE_ID, key=API_KEY, delay=RATE_LIMIT)[0]
+        return r
+    except Exception as e:
+        print(f"Error querying NVD database: {e}")
+        return None
 
 
-for i in range(len(data)):
-    # Get the values of those keys by querying the NVD database
-    nvd = addToDatabase(data[i]['cve'])
+def main():
+    data = read_json(INPUT_FILE)
+    final_data = [
+        {**item, **{
+            "sourceIdentifier": nvd.sourceIdentifier,
+            "cwe": nvd.cwe[0].value,
+            "baseScore": nvd.metrics.cvssMetricV30[0].cvssData.baseScore,
+            "baseSeverity": nvd.metrics.cvssMetricV30[0].cvssData.baseSeverity,
+            "scope": nvd.metrics.cvssMetricV30[0].cvssData.scope,
+            "vectorString": nvd.metrics.cvssMetricV30[0].cvssData.vectorString
+        }}
+        for item in data if (nvd := addToDatabase(item['cve'])) is not None
+    ]
 
-    print(nvd.metrics)
-    # Get the required keys from the NVD data (this can be changed to get more data if needed)
-    nvdData = {
-        "sourceIdentifier": nvd.sourceIdentifier,
-        "cwe": nvd.cwe[0].value,
-        "baseScore": nvd.metrics.cvssMetricV30[0].cvssData.baseScore,
-        "baseSeverity": nvd.metrics.cvssMetricV30[0].cvssData.baseSeverity,
-        "scope": nvd.metrics.cvssMetricV30[0].cvssData.scope,
-        "vectorString": nvd.metrics.cvssMetricV30[0].cvssData.vectorString
-    }
-
-    # Combine the two dictionaries
-    c = {**data[i], **nvdData}
-    # Add the values to the dictionary
-    final_data.append(c)
+    with open(OUTPUT_FILE, 'w') as file:
+        json.dump(final_data, file, indent=4)
 
 
-# Create a new JSON file with the updated data
-with open('./attackDB-final.json', 'w') as file:
-    json.dump(final_data, file, indent=4)
+if __name__ == "__main__":
+    main()
