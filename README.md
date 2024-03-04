e84352a425d258dc505b9b4309f6bea9339f5273
sid <siddhantkalgutkar@gmail.com>
2024-03-03 19:06:03 -0500

    Added test cases

--- README.md
+++ README.md
@@ -11,11 +11,15 @@ docker compose up
 
 ### Test API
 ```bash
-# Set the tests/attackDB.json
-# Set the tests/initState.json
 docker-compose up
 curl -X POST -H "Content-Type: application/json" -d '{"initState": {"paramA1":"xA", "paramA2":"yA"}}' http://127.0.0.1:5000/api/chains
-# Chains identified within attackDB.json will be stored in the 'chains' directory.
+```
+
+### Test Algorithm Locally
+```bash
+cd backend/algorithm-tests/
+# To run test case 1
+python3 test.py 1
 ```
 
 ### Flowchart
--- backend/app.py
+++ backend/app.py
@@ -13,29 +13,16 @@ def healthcheck():
     return "Server is Alive!"
 
 
-def log_and_save_chains(chains):
-    # Create a directory for the chains
-    os.makedirs("chains", exist_ok=True)
-
-    for i, chain in enumerate(chains):
-        chain_dicts = [attack.to_dict() for attack in chain]
-        # Create a filename for the chain
-        filename = f"chains/chain_{i+1}.json"
-        # Write the chain to the file
-        with open(filename, "w") as f:
-            json.dump(chain_dicts, f, indent=4)
-
-
 @app.route("/api/chains", methods=["POST"])
 def get_chains():
     # Get the initState from the request body
     initState_data = request.json.get("initState")
 
-    # Create a State object from the initState data
+    # Create a State object from the initState data 
     initState = State(initState_data)
 
-    # Load the attacks from the JSON file
-    attackDB = AttackDB(load_attacks("./tests/attackDB.json"))
+    # Load the attacks from the JSON file (get this data from the MongoDB in the future)
+    attackDB = AttackDB(load_attacks("./algorithm-tests/test-1/attackDB.json"))
 
     # Run the makeChain function with the loaded state and AttackDB
     chains = makeChain(initState, attackDB)
@@ -43,7 +30,7 @@ def get_chains():
     # Convert the chains to a list of dictionaries
     chains_dicts = [[attack.to_dict() for attack in chain] for chain in chains]
 
-    log_and_save_chains(chains)
+    return jsonify(chains_dicts)
 
 
 if __name__ == "__main__":
