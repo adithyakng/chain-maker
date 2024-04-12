0e40a66054d0d651829b772d063a0acfbece5355
Adithya KNG <adithyakng@gmail.com>
2024-04-12 19:14:39 -0400

    AvoidInfinteCycle

--- backend/CSVToJSON/csvToJson.py
+++ backend/CSVToJSON/csvToJson.py
@@ -18,7 +18,7 @@ info_gained = 16
 index = 0
 
 
-with open('entries.csv') as file:
+with open('/Users/adithya/Documents/CIS Spring 2024/project/chain-maker/backend/CSVToJSON/entries1.csv') as file:
     #read in the attacks csv file
     csv_reader = csv.reader(file)
 
@@ -39,30 +39,43 @@ with open('entries.csv') as file:
 
         #save prior state 
         prior_state_dict = {}
-        for state in attack[index_prior_state].split(';'):
+        for state in attack[index_prior_state].split(','):
+            state = state.strip()
             prior_state_dict[state] = state
         temp_dict['initState'] = prior_state_dict
 
         #save post state 
         post_state_dict = {}
-        for state in attack[index_post_state].split(';'):
+        for state in attack[index_post_state].split(','):
+            state = state.strip()
             post_state_dict[state] = state
         temp_dict['endState'] = post_state_dict
 
-        temp_dict['conference'] = attack[conference].split(';')
+        temp_dict['conference'] = attack[conference].split(',')
+        for(i, item) in enumerate(temp_dict['conference']):
+            temp_dict['conference'][i] = item.strip()
         
-        temp_dict['open_source_repo'] = attack[open_source_repo].split(';')
+        temp_dict['open_source_repo'] = attack[open_source_repo].split(',')
+        for(i, item) in enumerate(temp_dict['open_source_repo']):
+            temp_dict['open_source_repo'][i] = item.strip()
         
-        temp_dict['cve_number'] = attack[cve_number].split(';')
+        temp_dict['cve_number'] = attack[cve_number].split(',')
+        for(i, item) in enumerate(temp_dict['cve_number']):
+            temp_dict['cve_number'][i] = item.strip()
 
         #save prior knowledge
-        temp_dict['info_required'] = attack[info_required].split(';')
+        temp_dict['info_required'] = attack[info_required].split(',')
+        for(i, item) in enumerate(temp_dict['info_required']):
+            temp_dict['info_required'][i] = item.strip()
 
         #save post knowledge
-        temp_dict['info_required'] = attack[info_gained].split(';')
-
+        temp_dict['info_gained'] = attack[info_gained].split(',')
+        for(i, item) in enumerate(temp_dict['info_gained']):
+            temp_dict['info_gained'][i] = item.strip()
         #save hardware requirements
-        temp_dict['hardwareRequirements'] = attack[index_requ_hardware].split(';')
+        temp_dict['hardwareRequirements'] = attack[index_requ_hardware].split(',')
+        for(i, item) in enumerate(temp_dict['hardwareRequirements']):
+            temp_dict['hardwareRequirements'][i] = item.strip()
             
         
         #update the id with a hash of temp_dict
